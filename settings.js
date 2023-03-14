2e6200385e5a0385532ad4b20092ebc9569ab99e
RhmtCraft <94813145+RhmtCraft@users.noreply.github.com>
2023-03-14 20:59:32 +0700

    Update settings.js

--- settings.js
+++ settings.js
@@ -24,8 +24,8 @@ edit sewabot di file settings.js line 19
 global.firtsChat = true //send firts message to new user
 global.autoread = true // auto read message
 global.antidelete = false // anti delete
-global.antiviewonce = true // anti view once
-global.autobio = false //auto ganti bio
+global.antiviewonce = false // anti view once
+global.autobio = true //auto ganti bio
 global.anticall = true //anti call 
 global.userRegister = false
 global.autoblok212 = false //ubah jadi true biar nmr 212 di blok sama bot
@@ -41,18 +41,18 @@ global.language = indonesia //change indonesia to english if you don't understan
 
 // Other
 // Other
-global.botname = "Alphabot-Mdོ" //namabot kalian
-global.ownername = "ᴹᴿ᭄ ZeeoneOfcོ ×፝֟͜×" //nama kalian
+global.botname = "XellBot" //namabot kalian
+global.ownername = "Xell" //nama kalian
 global.myweb = "https://s.id/zeeoneofc" //bebas asal jan hapus
-global.youtube = "https://youtube.com/c/@ZeeoneOfc" //bebas asal jan hapus
+global.youtube = "https://youtube.com/c/@RhmtEditz" //bebas asal jan hapus
 global.myweb2 = "https://s.id/zeeoneofc" //bebas
-global.email = "zeeoneofc@gmail.com" //bebas
+global.email = "rahmatxell@gmail.com" //bebas
 global.region = "Indonesia" //bebas
 global.timezone = 'Asia/Jakarta' //  timezone wib
-global.premium = ['62887435047326'] //premium user
-global.owner = ["62887435047326", "62821931157232"] //ganti agar fitur owner bisa di gunakan
-global.ownernomer = "62887435047326" // nomor wa kalian
-global.ownernomerr = "+"+ownernomer //nmr wa kalian
+global.premium = ['6283867149253'] //premium user
+global.owner = ["6283867149253", "6287802572596"] //ganti agar fitur owner bisa di gunakan
+global.ownernomer = "6283867149253" // nomor wa kalian
+global.ownernomerr = "+6283867149253"+ownernomer //nmr wa kalian
 global.thumbnail = "./image/lol.jpg" // ini lol.jpg adalah nama foto di folder image. untuk foto bot
 global.donasi = "./image/donasi.jpg" // foto donasi di folder image
 global.background_welcome = "https://telegra.ph/file/edca4b513ddf29fd52c0e.jpg" // maks size 30kb, agar welcome image nya tdk delay
